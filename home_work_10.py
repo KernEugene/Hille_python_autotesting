581731e6f6d2bda3b22cb4ade155e5f81150ae08
Yevhenii Kern <kebbbba@gmail.com>
2022-01-20 12:04:29 +0200

    added  10 hm

---
+++ home_work_10.py
@@ -0,0 +1,27 @@
+class People:
+
+ def __init__(self, name, surname, age, gender):
+  self.name = name
+  self.surname = surname
+  self.age = age
+  self.gender = gender
+  print('Human has been created')
+
+  def eat(self):
+   pass
+
+  def sleep(self):
+   pass
+
+  def speak(self):
+   pass
+
+  def walk(self):
+   pass
+
+  def stay(self):
+   pass
+
+  def lie(self):
+   pass
+
