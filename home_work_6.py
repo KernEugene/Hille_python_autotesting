ad9d7382703d8336ca3c1ebc760a460493c12bdc
Yevhenii Kern <kebbbba@gmail.com>
2022-01-20 12:02:51 +0200

    added 6 and 10 hm

--- home_work_6.py
+++ home_work_6.py
@@ -1,17 +1,29 @@
 
+#Task1
+def voting():
+    num_votes = {}
+    for _ in range(int(input())):
+        candidate, votes = input().split()
+        num_votes[candidate] = num_votes.get(candidate, 0) + int(votes)
 
-# def was_it_before():
-#     new_str = "1 2 3 4 5 6 7 2 3 4"
-#     set_str = set()
-#     for number in new_str.split():
-#         if number in set_str:
-#             print("YES")
-#         else:
-#             print("NO")
-#
-# was_it_before()
+    for candidate, votes in sorted(num_votes.items()):
+        print(candidate, votes)
 
 
+#Task2
+
+counter = {}
+for i in range(int(input())):
+    line = input().split()
+    for word in line:
+        counter[word] = counter.get(word, 0) + 1
+
+max_count = max(counter.values())
+most_frequent = [k for k, v in counter.items() if v == max_count]
+print(min(most_frequent))
+
+
+#Task3
 
 numbers = [int(s) for s in input().split()]
 occur_before = set()
